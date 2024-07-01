e23130bbb92857fda96d0a3321f7f6fcdfc6919c
jlbuild <juliabuildbot@gmail.com>
2024-07-01 11:19:53 +0000

    FLEXPART_jll build 10.4.2+0

--- src/wrappers/x86_64-linux-gnu-libgfortran5.jl
+++ src/wrappers/x86_64-linux-gnu-libgfortran5.jl
@@ -4,10 +4,11 @@ export FLEXPART
 using CompilerSupportLibraries_jll
 using eccodes_jll
 using NetCDFF_jll
+using JasPer_jll
 JLLWrappers.@generate_wrapper_header("FLEXPART")
 JLLWrappers.@declare_executable_product(FLEXPART)
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, eccodes_jll, NetCDFF_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, eccodes_jll, NetCDFF_jll, JasPer_jll)
     JLLWrappers.@init_executable_product(
         FLEXPART,
         "bin/FLEXPART",
